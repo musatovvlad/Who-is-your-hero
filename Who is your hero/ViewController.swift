9182005d352337b8677c9b36898d488c9da9fc97
Musatov Vladimir <vladimir.pmz@gmail.com>
2022-10-21 21:53:05 +0300

    Initial Commit

---
+++ Who is your hero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Who is your hero
+//
+//  Created by Vladimir on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
