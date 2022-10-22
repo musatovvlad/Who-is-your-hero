78a68b8ce0b34dd96b3cf3e3545e1f7e8ca05bd1
Musatov Vladimir <vladimir.pmz@gmail.com>
2022-10-22 12:38:33 +0300

    designed welcome screen and add image in Assets.xcassets

--- Who is your hero/WelcomeViewController.swift
+++ Who is your hero/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Who is your hero
 //
 //  Created by Vladimir on 21.10.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
