a574c6f366eeb816f5070f7eed8275a71eb78087
Musatov Vladimir <vladimir.pmz@gmail.com>
2022-11-01 14:05:40 +0300

    added the action function radio buttons checkboxes

--- Who is your hero/view controllers/QuestionViewController.swift
+++ Who is your hero/view controllers/QuestionViewController.swift
@@ -8,22 +8,76 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    @IBOutlet weak var brownButton: UIButton!
+    
+    @IBOutlet weak var blondButton: UIButton!
+    
+    @IBOutlet weak var redButton: UIButton!
+    
+    @IBOutlet weak var lightBrownButton: UIButton!
+    
+    @IBOutlet weak var grayHairedButton: UIButton!
+    
+    @IBOutlet weak var brunette: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func radioButtonCheckboxAction(_ sender: UIButton) {
+    
+    if sender.tag == 1{
+        brownButton.isSelected = true
+        blondButton.isSelected = false
+        redButton.isSelected = false
+        lightBrownButton.isSelected = false
+        grayHairedButton.isSelected = false
+        brunette.isSelected = false
     }
-    */
+else if sender.tag == 2 {
+    brownButton.isSelected = false
+    blondButton.isSelected = true
+    redButton.isSelected = false
+    lightBrownButton.isSelected = false
+    grayHairedButton.isSelected = false
+    brunette.isSelected = false
+}
+    else if sender.tag == 3 {
+        brownButton.isSelected = false
+        blondButton.isSelected = false
+        redButton.isSelected = true
+        lightBrownButton.isSelected = false
+        grayHairedButton.isSelected = false
+        brunette.isSelected = false
+    }
+    else if sender.tag == 4 {
+        brownButton.isSelected = false
+        blondButton.isSelected = false
+        redButton.isSelected = false
+        lightBrownButton.isSelected = true
+        grayHairedButton.isSelected = false
+        brunette.isSelected = false
+    }
+    else if sender.tag == 5 {
+        brownButton.isSelected = false
+        blondButton.isSelected = false
+        redButton.isSelected = false
+        lightBrownButton.isSelected = false
+        grayHairedButton.isSelected = true
+        brunette.isSelected = false
+    }
+    else if sender.tag == 6 {
+        brownButton.isSelected = false
+        blondButton.isSelected = false
+        redButton.isSelected = false
+        lightBrownButton.isSelected = false
+        grayHairedButton.isSelected = false
+        brunette.isSelected = true
+    }
+        print(sender.tag)
 
+
+}
 }
